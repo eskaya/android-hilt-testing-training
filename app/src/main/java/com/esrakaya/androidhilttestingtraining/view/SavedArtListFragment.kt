66fe86d4fc68c40a124252a22fa7453f37ce3220
Esra Kaya <esra.kaya@bil.omu.edu.tr>
2024-04-19 15:06:32 +0300

    Create nav graph.

--- app/src/main/java/com/esrakaya/androidhilttestingtraining/view/SavedArtListFragment.kt
+++ app/src/main/java/com/esrakaya/androidhilttestingtraining/view/SavedArtListFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.esrakaya.androidhilttestingtraining.R
+import com.esrakaya.androidhilttestingtraining.databinding.FragmentSavedArtsListBinding
 
-class SavedArtListFragment:Fragment(R.layout.fragment_saved_arts_list) {
+class SavedArtListFragment : Fragment(R.layout.fragment_saved_arts_list) {
 
+    private lateinit var binding: FragmentSavedArtsListBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSavedArtsListBinding.bind(view)
+
+        binding.fab.setOnClickListener {
+            //make navigation
+        }
     }
 }
