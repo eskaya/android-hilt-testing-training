f856e7c407bfece38cdb621de2124793bf7db273
Esra Kaya <esra.kaya@bil.omu.edu.tr>
2024-04-21 20:02:09 +0300

    Fixed navigation component.

--- app/src/main/java/com/esrakaya/androidhilttestingtraining/view/SavedArtListFragment.kt
+++ app/src/main/java/com/esrakaya/androidhilttestingtraining/view/SavedArtListFragment.kt
@@ -3,6 +3,7 @@ package com.esrakaya.androidhilttestingtraining.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.esrakaya.androidhilttestingtraining.R
 import com.esrakaya.androidhilttestingtraining.databinding.FragmentSavedArtsListBinding
 
@@ -15,7 +16,8 @@ class SavedArtListFragment : Fragment(R.layout.fragment_saved_arts_list) {
         binding = FragmentSavedArtsListBinding.bind(view)
 
         binding.fab.setOnClickListener {
-            //make navigation
+          //  Navigation.findNavController(view).navigate(R.id.action_addArtFragment_to_imageApiFragment) err
+            findNavController().navigate(R.id.action_savedArtListFragment_to_addArtFragment)
         }
     }
 }
