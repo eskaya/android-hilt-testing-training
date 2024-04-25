ff3fe89ab93ffd10a924cb683d256f0e8dae68ab
Esra Kaya <esra.kaya@bil.omu.edu.tr>
2024-04-25 14:32:11 +0300

    Fixed back state for add art page.

--- app/src/main/java/com/esrakaya/androidhilttestingtraining/view/AddArtFragment.kt
+++ app/src/main/java/com/esrakaya/androidhilttestingtraining/view/AddArtFragment.kt
@@ -2,15 +2,27 @@ package com.esrakaya.androidhilttestingtraining.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.esrakaya.androidhilttestingtraining.R
 import com.esrakaya.androidhilttestingtraining.databinding.FragmentAddArtBinding
 
-class AddArtFragment: Fragment(R.layout.fragment_add_art) {
+class AddArtFragment : Fragment(R.layout.fragment_add_art) {
     private lateinit var binding: FragmentAddArtBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentAddArtBinding.bind(view)
+        binding.btnArtSave.setOnClickListener {
+            findNavController().navigate(R.id.action_addArtFragment_to_imageApiFragment)
+        }
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+
+        }
     }
 }
