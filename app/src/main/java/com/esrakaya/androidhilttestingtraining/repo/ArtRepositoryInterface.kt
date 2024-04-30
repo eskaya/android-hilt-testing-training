274aa95055f761af21764583d8c0a89c76082649
Esra Kaya <esra.kaya@bil.omu.edu.tr>
2024-04-30 16:27:12 +0300

    Create ArtRepositoryInterface.

---
+++ app/src/main/java/com/esrakaya/androidhilttestingtraining/repo/ArtRepositoryInterface.kt
@@ -0,0 +1,19 @@
+package com.esrakaya.androidhilttestingtraining.repo
+
+import androidx.lifecycle.LiveData
+import com.esrakaya.androidhilttestingtraining.model.ImageResponse
+import com.esrakaya.androidhilttestingtraining.room.model.Art
+import com.esrakaya.androidhilttestingtraining.util.Resource
+
+interface ArtRepositoryInterface {
+
+    suspend fun insertArt(art: Art)
+
+    suspend fun deleteArt(art: Art)
+
+    fun getArt(): LiveData<List<Art>>
+    
+    //retrofit test için, artık dönen veriler bir rResource tipinin içerisinde olacak
+    suspend fun searchImage(key: String): Resource<ImageResponse>
+
+}
